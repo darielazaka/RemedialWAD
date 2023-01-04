acd81c087aba5e3815a89d26919c127139d84f41
darielazaka <101590031+darielazaka@users.noreply.github.com>
2023-01-04 13:17:31 +0700

    Revert "Remedialnya"
    This reverts commit ce675831502cee744ede4974c86a98c95141f711.

--- MODUL4 DARIEL REMEDIAL/modules/KONEKSINYAKAKAK.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "localhost";$username = "root";$password = "";$database = "vapestore_dariel";
-
-$conn = new mysqli($servername, $username, $password, $database);
-$connect = mysqli_connect($servername, $username, $password, $database);
-
-?>
